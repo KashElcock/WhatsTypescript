d3ba612ff42c3d1d8a6a58fa8efaaccec9606efe
KashElcock <116089696+KashElcock@users.noreply.github.com>
2022-12-12 01:31:30 -0500

    trying fetch, made config file

--- index.ts
+++ index.ts
@@ -1,15 +1,28 @@
 // Import stylesheets
 import './style.css';
 
-
+const url = 'https://api.dictionaryapi.dev/api/v2/entries/en_US/';
 const form: HTMLFormElement = document.querySelector('#defineform');
 
 
-form.onsubmit = () => {
+form.onsubmit = (event) => {
   const formData = new FormData(form);
 
   console.log(formData);
   const text = formData.get('defineword') as string;
   console.log(text);
+  fetch(url + text) // fetch returns a promise
+    .then(response => response.json()) // convert to json
+    .then(data => {
+      console.log(data);
+      const definition = data[0].meanings[0].definitions[0].definition;
+      const partOfSpeech = data[0].meanings[0].partOfSpeech;
+      const word = data[0].word;
+      const output = document.querySelector('#output');
+      output.innerHTML = `<h2>${word} (${partOfSpeech})</h2><p>${definition}</p>`;
+    })
+    .catch(error => console.error(error));
+  event.preventDefault(); // prevent default form submission
+  
   return false; // prevent reload
 };
